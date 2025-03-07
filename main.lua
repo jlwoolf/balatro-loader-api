d0497219b7b555ea8bfcf12511e27ac0c0d08393
jonathan <jonathan@jlwoolf.com>
2025-03-07 12:45:58 -0700

    Removed SMODS.INIT as that is outdated

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
 ---@diagnostic disable: param-type-mismatch
 LOADER_API = {}
 
-function SMODS.INIT.LOADER_API()
-	local mod = SMODS.findModByID("loader-api")
-	assert(load(NFS.read(mod.path .. "src/utils.lua")))()
+local mod = SMODS.findModByID("loader-api")
+assert(load(NFS.read(mod.path .. "src/utils.lua")))()
 
-	LOADER_API.init(mod.id)
-end
+LOADER_API.init(mod.id)
